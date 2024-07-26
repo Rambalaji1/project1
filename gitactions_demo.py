21e9fe82cee0307004015c8a10e5854d3fa740f6
Rambalaji1 <rambala1999@gmail.com>
2024-07-26 16:28:31 +0530

    first commit

---
+++ gitactions_demo.py
@@ -0,0 +1,19 @@
+def is_palindrome(s):
+  """Checks if a string is a palindrome.
+  
+  Args:
+    s: The string to check.
+  
+  Returns:
+    True if the string is a palindrome, False otherwise.
+  """
+  return s == s[::-1]
+
+string = input("Enter a string: ")
+
+if is_palindrome(string):
+  print(f"{string} is a palindrome.")
+else:
+  print(f"{string} is not a palindrome.")
+  print("so its wrong")
+  print('Wrong again!')
